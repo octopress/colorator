ae86344cb6cce845c52e247f325a5e39821360aa
Parker Moore <parkrmoore@gmail.com>
2013-04-13 18:57:41 +0200

    Add license, rakefile, lib/

---
+++ lib/colorator.rb
@@ -0,0 +1,16 @@
+module Colorator
+  VERSION = "0.1"
+  ANSI_COLORS = {
+    :black   => 30,
+    :red     => 31,
+    :green   => 32,
+    :yellow  => 33,
+    :blue    => 34,
+    :magenta => 35,
+    :cyan    => 36,
+    :white   => 37,
+    :bold    => 1
+  }
+end
+
+require "colorator/core_ext"
